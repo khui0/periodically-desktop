4f10089dc473754c9cc5170dfce983b6ae1cb045
Kenny <101839505+khui0@users.noreply.github.com>
2024-04-07 22:26:20 -0400

    fix: time zone offset

--- src/renderer/src/lib/time.ts
+++ src/renderer/src/lib/time.ts
@@ -70,7 +70,7 @@ export function getDeltaDay(date: string): number {
   const DAY: number = 86_400_000;
   const TIMEZONE_OFFSET: number = new Date().getTimezoneOffset() * 60_000;
   const target: number = Math.floor((new Date(date).getTime() - TIMEZONE_OFFSET) / DAY);
-  const now: number = Math.floor(Date.now() / DAY);
+  const now: number = Math.floor((Date.now() - TIMEZONE_OFFSET) / DAY);
   return target - now;
 }
 
